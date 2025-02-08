9eee950ae9137d746314f7810c302bdb1e52587b
jack-chapman <jackchapman093@gmail.com>
2025-02-08 23:23:24 +0000

    minor code tweaks

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,6 @@
 const movieResultsElement = document.getElementById("movie-results")
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
-let movieResults = []
-
 
 // Prompt to use search
 movieResultsElement.innerHTML = ` 
@@ -14,24 +12,30 @@ movieResultsElement.innerHTML = `
 //No searches found message
 function renderNoResultsMessage() {
     movieResultsElement.innerHTML = ` 
-    <div class="no-search-placeholder"> 
-    <i class="fa-solid fa-film"></i> <p>No results found, sorry!</p>
-</div>
+        <div class="no-search-placeholder"> 
+            <i class="fa-solid fa-film"></i>
+            <p>No results found, sorry!</p>
+        </div>
 `
 }
 
 //Loading message
 function renderLoadingMessage() {
     movieResultsElement.innerHTML = `
-    <div class="loading-placeholder">
-    <i class="fa-solid fa-spinner"></i> <p> Result are loading... </p>
-    </div>
-    `
+        <div class="loading-placeholder">
+            <i class="fa-solid fa-spinner"></i>
+            <p> Result are loading... </p>
+        </div>
+`
 }
 
 function searchMovieDatabase() {
     renderLoadingMessage()
-    fetch(`https://www.omdbapi.com/?apikey=863d41df&s=${searchInput.value}&type=movie`)
+    let requestUrl = `https://www.omdbapi.com/?apikey=863d41df&type=movie`
+    if (searchInput.value) {
+        requestUrl = `https://www.omdbapi.com/?apikey=863d41df&s=${searchInput.value}&type=movie`
+    }
+    fetch(requestUrl)
         .then(response => response.json())
         .then(data => {
             if (data.Response === "False") {
@@ -56,10 +60,10 @@ function getMovieDetails(moviesArr) {
     })
     Promise.all(requests)
         .then(responses => {
-            movieResults = responses.filter((movie) => {
+            const movieResults = responses.filter((movie) => {
                 return movie.Title !== "N/A" && movie.Poster !== "N/A"
             })
-            renderMovieResults()
+            renderMovieResults(movieResults)
         })
 }
 
@@ -73,15 +77,16 @@ function getSavedMovies() {
     return movieWatchlist
 }
 
-function saveToWatchlist(filmID) {
-
+function saveToWatchlist(filmID, movieResults) {
     const movieWatchlist = getSavedMovies()
+
     const alreadyInWatchlist = movieWatchlist.find((movie) => {
         return movie.imdbID === filmID
     })
     if (alreadyInWatchlist) {
         return
     }
+
     const foundMovie = movieResults.find((movie) => {
         return movie.imdbID === filmID
     })
@@ -89,18 +94,17 @@ function saveToWatchlist(filmID) {
     movieWatchlist.push(foundMovie)
     const savedMovies = JSON.stringify(movieWatchlist)
     localStorage.setItem("Watchlist", savedMovies)
-
 }
 
-function renderMovieResults() {
-
+function renderMovieResults(movieResults) {
     const addToWatchlistBtnInnerHtml = `<i class="fa-solid fa-circle-plus"></i> Watchlist `
     const alreadySavedBtnInnerHtml = `<i class="fa-solid fa-circle-check"></i> Added to Watchlist`
 
     const movieWatchlist = getSavedMovies()
-    let htmlString = ""
-    movieResults.forEach(data => {
 
+    let htmlString = ""
+
+    movieResults.forEach(data => {
         const alreadyInWatchlist = movieWatchlist.find((movie) => {
             return movie.imdbID === data.imdbID
         })
@@ -114,26 +118,31 @@ function renderMovieResults() {
             </div>
             <div class="movie-details">    
                 <div class="movie-title">      
-                        <h2> ${data.Title}</h2>
-                        <p> ${data.Ratings[0] === undefined ? "Rating unavailable" : data.Ratings[0]?.Value.split("/")[0]} <i class="fa-solid fa-star"></i></p>
+                    <h2> ${data.Title}</h2>
+                    <p>
+                        ${data.Ratings[0] === undefined ? "Rating unavailable" : data.Ratings[0]?.Value.split("/")[0]}
+                        <i class="fa-solid fa-star"></i>
+                    </p>
                 </div>
-                        <div class="small-detail">
-                            <p> ${data.Runtime === "N/A" ? "Runtime unavailable" : data.Runtime} </p>
-                            <p> ${data.Genre === "N/A" ? "Genre unavailable" : data.Genre} </p>
-                            
-                            <button data-add-button data-add-id="${data.imdbID}" class="watchlist-btn"> ${btnInnerHtml} </button>
-                        </div>
-                        <div class="about-detail"> 
-                            <p>${data.Plot === "N/A" ? "No description available" : data.Plot}</p> 
-                        </div> 
+                <div class="small-detail">
+                    <p> ${data.Runtime === "N/A" ? "Runtime unavailable" : data.Runtime} </p>
+                    <p> ${data.Genre === "N/A" ? "Genre unavailable" : data.Genre} </p>
+                    
+                    <button data-add-button data-add-id="${data.imdbID}" class="watchlist-btn"> ${btnInnerHtml} </button>
                 </div>
-            </div>    
-                `
+                <div class="about-detail"> 
+                    <p>${data.Plot === "N/A" ? "No description available" : data.Plot}</p> 
+                </div> 
+            </div>
+        </div>    
+        `
     })
+
     movieResultsElement.innerHTML = htmlString
+
     document.querySelectorAll("[data-add-button]").forEach(button => {
         button.addEventListener('click', () => {
-            saveToWatchlist(button.dataset.addId)
+            saveToWatchlist(button.dataset.addId, movieResults)
             button.innerHTML = alreadySavedBtnInnerHtml
         })
     })
--- scripts/watchlist.js
+++ scripts/watchlist.js
@@ -1,34 +1,23 @@
 const watchlistResultsElement = document.getElementById("watchlist-results")
 const searchInput = document.getElementById("search-input")
 const searchBtn = document.getElementById("search-btn")
-let movieResults = JSON.parse(localStorage.getItem("Watchlist"))
-
-renderWatchlist()
 
 function renderWatchlist() {
+    const movieResults = JSON.parse(localStorage.getItem("Watchlist"))
     if (movieResults.length === 0) {
         watchlistResultsElement.innerHTML = ` 
-        <div class="watchlist-placeholder"> 
-            <p>Your watchlist is looking a little empty...</p>
-             
-             <a href="index.html"> <i class="fa-solid fa-circle-plus"> </i> Let's add some movies!</a>
-        </div>
-    `
+            <div class="watchlist-placeholder"> 
+                <p>Your watchlist is looking a little empty...</p>
+                 
+                <a href="index.html">
+                    <i class="fa-solid fa-circle-plus"> </i>
+                    Let's add some movies!
+                </a>
+            </div>
+        `
     } else {
-        renderWatchlistResults()
+        renderWatchlistResults(movieResults)
     }
-}
-
-
-function saveToWatchlist(filmID) {
-    const foundMovie = movieResults.find((movie) => {
-        return movie.imdbID === filmID
-    })
-
-    const moviesToSave = [foundMovie]
-    const savedMovies = JSON.stringify(moviesToSave)
-    localStorage.setItem("Watchlist", savedMovies)
-
 }
 
 function removeMovieFromLocal(filmID) {
@@ -44,14 +33,14 @@ function removeMovieFromLocal(filmID) {
     })
 
     movieWatchlist.splice(foundMovieIndex, 1)
-    console.log(movieWatchlist)
+
     const savedMovies = JSON.stringify(movieWatchlist)
     localStorage.setItem("Watchlist", savedMovies)
-    movieResults = JSON.parse(localStorage.getItem("Watchlist"))
+
     renderWatchlist()
 }
 
-function renderWatchlistResults() {
+function renderWatchlistResults(movieResults) {
     let htmlString = ""
     movieResults.forEach(data => {
         htmlString += `
@@ -62,22 +51,32 @@ function renderWatchlistResults() {
             <div class="movie-details">          
                 <div class="movie-title">      
                     <h2> ${data.Title}</h2>
-                        <p> ${data.Ratings[0] === undefined ? "Rating unavailable" : data.Ratings[0]?.Value.split("/")[0]} <i class="fa-solid fa-star"></i></p>
+                    <p>
+                        ${data.Ratings[0] === undefined ? "Rating unavailable" : data.Ratings[0]?.Value.split("/")[0]}
+                        <i class="fa-solid fa-star"></i>
+                    </p>
                 </div>
-                        <div class="small-detail">
-                            <p> ${data.Runtime === "N/A" ? "Runtime unavailable" : data.Runtime} </p>
-                            <p> ${data.Genre === "N/A" ? "Genre unavailable" : data.Genre} </p>
-                            <button data-add-button data-add-id="${data.imdbID}" class="remove-btn"> <i class="fa-solid fa-circle-minus"> </i> Remove </button>
-                        </div>
-                        <div class="about-detail"> 
-                            <p>${data.Plot === "N/A" ? "No description available" : data.Plot}</p> 
-                        </div> 
+                <div class="small-detail">
+                    <p> ${data.Runtime === "N/A" ? "Runtime unavailable" : data.Runtime} </p>
+                    <p> ${data.Genre === "N/A" ? "Genre unavailable" : data.Genre} </p>
+                    <button data-remove-button data-add-id="${data.imdbID}" class="remove-btn">
+                        <i class="fa-solid fa-circle-minus"> </i>
+                        Remove
+                    </button>
                 </div>
-            </div>    
-                `
+                <div class="about-detail"> 
+                    <p>${data.Plot === "N/A" ? "No description available" : data.Plot}</p> 
+                </div> 
+            </div>
+        </div>    
+        `
     })
+
     watchlistResultsElement.innerHTML = htmlString
-    document.querySelectorAll("[data-add-button]").forEach(button => {
+
+    document.querySelectorAll("[data-remove-button]").forEach(button => {
         button.addEventListener('click', () => removeMovieFromLocal(button.dataset.addId))
     })
 }
+
+renderWatchlist()
